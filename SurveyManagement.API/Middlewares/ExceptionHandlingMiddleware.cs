3c84db9c5929b6ee6ab34a0dd890506dea4cc731
merve.bayrak <merve.bayrak@flo.com.tr>
2025-07-24 10:24:13 +0300

    NotFoundException using kısmı düzeltmeler

--- SurveyManagement.API/Middlewares/ExceptionHandlingMiddleware.cs
+++ SurveyManagement.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -2,7 +2,7 @@
 using SurveyManagement.Application.Wrappers;
 using System.Net;
 using System.Text.Json;
-using SurveyManagement.Application.Exceptions;
+using SurveyManagement.Domain.Exceptions;
 
 namespace SurveyManagement.API.Middlewares
 {
